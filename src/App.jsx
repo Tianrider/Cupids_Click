d86c7239fd7a8147131b5e97f7823591292a430c
Tianrider <hadiwijayachristian7@gmail.com>
2024-02-12 16:33:04 +0700

    first commit, basic text logic

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const texts = [
+    "ini text pertama, i love you beb <3",
+    "ini text kedua, jadian yuk",
+    "ini text ketiga, mau gak??"
+  ]
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <h1 id='text'>{texts[count]}</h1>
+    </>
+  )
+}
+
+export default App
