e9cc5a98cdaf390b1295eb95f7027c46a3c1cb93
Jonathan Kosasih <152432942+JonathanKosasih18@users.noreply.github.com>
2024-02-15 23:07:09 +0700

    Added BG and character, audio still weird (#9)
    * Added chara and BG, audio still broken
    
    * BG, Audio still broken

--- src/pages/NoTwo.jsx
+++ src/pages/NoTwo.jsx
@@ -4,51 +4,78 @@ import { useState } from "react";
 import ChoicesBox from "../components/ChoicesBox";
 import TextBox from "../components/TextBox";
 import DialogBox from "../components/DialogBox";
-import ReactAudioPlayer from "react-audio-player";
-import { useEffect } from "react";
 import song from "../assets/sad-song.mp3";
+import ReactAudioPlayer from "react-audio-player";
+import background from "../assets/background-no-two.png";
+import chara from "../assets/chara-no-two.png";
 
 function NoTwo() {
-   const [count, setCount] = useState(0);
-   const [choices, setChoices] = useState(false);
+    const [count, setCount] = useState(0);
+    const [choices, setChoices] = useState(false);
 
-   const texts = ["Guess what? You got ME!"];
+    const texts = [
+        "Guess what?",
+        "You got ME!"
+    ];
 
-   const handleIncrement = () => {
-      if (count >= texts.length - 1) {
-         setChoices(true);
-      }
-      if (!choices && count < texts.length - 1) {
-         setCount(count + 1);
-      }
-   };
+    const handleIncrement = () => {
+        if (count >= texts.length - 1) {
+            setChoices(true);
+        }
+        if (!choices && count < texts.length - 1) {
+            setCount(count + 1);
+        }
+    };
 
-   useEffect(() => {
-      const audio = new Audio(song);
-      audio.play();
-   }, []);
+    const handleDecrement = () => {
+        if (count <= 0) {
+            return;
+        }
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    };
 
-   return (
-      <>
-         <div>
-            <ReactAudioPlayer src={song} autoPlay loop />
-         </div>
+    return (
+        <>
+            <img
+                src={background}
+                alt="background"
+                className="h-screen w-screen z-[-2] absolute top-0 left-0"
+            />
+            <img
+                src={chara}
+                alt="chara"
+                className="h-[90vh] md:h-[94vh] absolute bottom-0 z-[-1]"
+            />
 
-         <div className="h-dvh w-screen items-center justify-start gap-8 flex flex-col-reverse bg-blue-300">
-            <TextBox texts={texts} count={count} />
+            <div> // fix audio here
+                <ReactAudioPlayer
+                    src={song}
+                    autoPlay
+                    loop
+                />
+            </div>
 
-            <DialogBox handleIncrement={handleIncrement} />
+            <div className="h-dvh w-screen items-center justify-start gap-8 flex flex-col-reverse">
+                <TextBox texts={texts} count={count} />
 
-            {choices && (
-               <ChoicesBox
-                  textYes="Mauu"
-                  textNo="IUW NAJIS"
-                  hrefNo="/no-three"
-               />
-            )}
-         </div>
-      </>
-   );
+                <DialogBox 
+                    handleIncrement={handleIncrement} 
+                    handleDecrement={handleDecrement}
+                />
+
+                {choices && (
+                    <ChoicesBox
+                        textYes="Mauu"
+                        hrefYes="/good-ending"
+                        textNo="IUW NAJIS"
+                        hrefNo="/no-three"
+                    />
+                )}
+            </div>
+        </>
+    );
 }
 
 export default NoTwo;
