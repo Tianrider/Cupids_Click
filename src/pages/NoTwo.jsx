dfef848d69e377762859fa7736635de991703fea
JonathanKosasih18 <jonathankosasih@gmail.com>
2024-02-15 16:04:56 +0700

    Audio somewhat working

--- src/pages/NoTwo.jsx
+++ src/pages/NoTwo.jsx
@@ -4,7 +4,9 @@ import { useState } from "react";
 import ChoicesBox from "../components/ChoicesBox";
 import TextBox from "../components/TextBox";
 import DialogBox from "../components/DialogBox";
-
+import ReactAudioPlayer from "react-audio-player";
+import { useEffect } from "react";
+import song from "../assets/sad-song.mp3"
 
 function NoTwo() {
     const [count, setCount] = useState(0);
@@ -21,10 +23,20 @@ function NoTwo() {
         }
     };
 
+    useEffect(() => {
+        const audio = new Audio(song);
+        audio.play();
+    }, []);
+
     return (
         <>
-            
-
+            <div>
+                <ReactAudioPlayer
+                src={song}
+                autoPlay
+                loop
+                />
+            </div>
 
             <div className="h-dvh w-screen items-center justify-start gap-8 flex flex-col-reverse bg-blue-300">
                 <TextBox texts={texts} count={count} />
